d50759178124e537f9d6dd03e471e811e41ca1b2
Robert Olsen <robert.olsen@halmstad.se>
2019-08-30 12:49:29 +0200

    Lagt till en knapp

--- WF/Form1.cs
+++ WF/Form1.cs
@@ -16,5 +16,10 @@ namespace WF
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
